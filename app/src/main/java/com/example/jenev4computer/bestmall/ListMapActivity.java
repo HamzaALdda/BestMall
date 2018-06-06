bdee0a7d0d3f37076a637954caf372bb2004624b
Süleyman Özkal <suleymanzkl94@gmail.com>
2018-06-07 00:09:33 +0200

    Recycle Layoutouts are done
    dummy data 2

--- app/src/main/java/com/example/jenev4computer/bestmall/ListMapActivity.java
+++ app/src/main/java/com/example/jenev4computer/bestmall/ListMapActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +18,9 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListMapActivity extends AppCompatActivity {
 
     private LinearLayout linearLayout;
@@ -24,12 +29,38 @@ public class ListMapActivity extends AppCompatActivity {
     private ImageButton btn_search;
     private TextView textView_input;
 
+    private RecyclerView recyclerView;
+    private MallAdapter mallAdapter;
+    private List<Mall> mallList;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_map);
+
+        //-------------RecyclerView ------------------------
+        mallList = new ArrayList<>();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewMall);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mallList.add(
+                new Mall( 1, "Forum Gummersbach", "", "NRW",
+                        "Gummersbach",51643, "Steinmülleralle 5",
+                        1, 51.024560, 7.565274)
+        );
+        mallList.add(
+                new Mall (2,"Köln Arcaden", "", "NRW",
+                        "Köln", 51103, "Kalker Hauptstrasse 55",
+                        1,50.938064, 6.997962)
+        );
+
+        mallAdapter = new MallAdapter(this, mallList);
+        recyclerView.setAdapter(mallAdapter);
+
 
         textView_input = (EditText) findViewById(R.id.listView_textField_input);
 
@@ -60,6 +91,9 @@ public class ListMapActivity extends AppCompatActivity {
 
             }
         });
+
+
+
 
     }
 
