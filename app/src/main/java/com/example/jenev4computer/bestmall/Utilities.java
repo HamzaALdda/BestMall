0360ff041278758975d3470c9b9e81a074f58060
Amjad <amjad_smith@yahoo.com>
2018-06-06 17:30:54 +0200

    Distance Formula Code

--- app/src/main/java/com/example/jenev4computer/bestmall/Utilities.java
+++ app/src/main/java/com/example/jenev4computer/bestmall/Utilities.java
@@ -1,6 +1,10 @@
 package com.example.jenev4computer.bestmall;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
+
 public class Utilities {
+
 
     public double distance(double lat1, double lon1, double lat2, double lon2) {
         double theta = lon1 - lon2;
@@ -8,56 +12,21 @@ public class Utilities {
         dist = Math.acos(dist);
         dist = rad2deg(dist);
         dist = dist * 60 * 1.1515;
-        
+
         return dist * 1.609344;
     }
 
-    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    /*::  This function converts decimal degrees to radians             :*/
-    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
     private double deg2rad(double deg) {
         return (deg * Math.PI / 180.0);
     }
 
-    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    /*::  This function converts radians to decimal degrees             :*/
-    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
     private double rad2deg(double rad) {
         return (rad * 180.0 / Math.PI);
     }
 
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-public class Utilities {
-
-
-    public double calcDistBetween2Coords() {
-        double x1, x2, y1, y2;
-        double dis;
-        x1 = 1;
-        x2 = 1;
-        y1 = 4;
-        y2 = 6;
-        dis = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-        return dis;
-     }
-}
-
-*/
 
